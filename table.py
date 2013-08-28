d3ea5089065cf05694bec6c7bf621cbc6ce5cb58
Иван Капукаранов <ivankapukaranov@gmail.com>
2013-08-28 14:27:42 +0300

    No need for this class

--- table.py
+++
@@ -1,8 +0,0 @@
-import pygame
-from settings import *
-
-class Table():
-    table = pygame.image.load('Snooker_table3.png')
-    def __init__(self):
-        #self.table_surface = pygame.Surface(TABLE_SIZE)
-        self.table = pygame.image.load('Snooker_table3.png')
